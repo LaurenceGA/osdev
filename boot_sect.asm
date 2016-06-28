51a7ae5e96f5bd2d4072fdcf3c070fef8fccd849
Laurence Armstrong <lorryarmstrong@gmail.com>
2016-06-28 22:50:58 +1200

    Boot sector now loads data from later in disk. Print_hex can now be used properly multiple times.

--- boot_sect.asm
+++ boot_sect.asm
@@ -2,10 +2,25 @@
 ; A simple boot sector program that says hello and then goodbye
 ;
 [org 0x7c00]
+	mov bp, 0x8000		; Move the stack out of the way
+	mov sp, bp
+
 	mov bx, hello_message	; Print hello
 	call print_string
 
-	mov ax, 0x0af2
+	mov [BOOT_DRIVE], dl
+
+	mov bx, 0x9000		; 5 secotrs 0x0000(ES):0x9000(BX)
+	mov dh, 5
+	mov dl, [BOOT_DRIVE]
+	call disk_load
+
+
+
+	mov ax, [0x9000]	; Print first laoded word
+	call print_hex
+
+	mov ax, [0x9000+512]	; Print first laoded word
 	call print_hex
 
 	mov bx, goodbye_message	; Print goodbye
@@ -15,15 +30,22 @@
 
 %include "print_string.asm"	; Make sure we can print strings
 %include "print_hex.asm"
+%include "disk_load.asm"
 
 ; data
-CR equ 0xd	; '\r'
-LF equ 0xa	; '\n'
+BOOT_DRIVE: db 0
+
+CR: equ 0xd	; '\r'
+LF: equ 0xa	; '\n'
 hello_message:
-	db 'Hi there!', CR, LF, 0
+	db 'Hi there!', CR, LF, 'Loading from disk...', CR, LF, 0
 goodbye_message:
 	db 'Bye!', CR, LF, 0
 
 times 510-($-$$) db 0	; pad program out to 510th byte
-
 dw 0xaa55	; Magic number tells the boot loader that this is a boot sector
+
+; Make up some data
+times 256 dw 0xdada
+times 256 dw 0xface
+times 2048 dw 0
--- print_hex.asm
+++ print_hex.asm
@@ -23,6 +23,19 @@ endloop:
 	mov bx, hex_string
 	call print_string
 
+	; Now we need to reset the string so it can be used again
+	mov bx, 5
+
+resetloop:
+	cmp bx, 2
+	jl return
+
+	mov byte [hex_string+bx], '0'	; Set to '0'
+
+	add bx, -1
+	jmp resetloop
+
+return:
 	popa
 	ret
 ; Data
