e2505aa2b41a171569a89f70381a34b29a9982cf
Pyxxil <josh.hill189@gmail.com>
2016-07-05 16:55:23 +1200

    Fixed itoa returning '0' for any value if the base isn't 10.

--- libc/src/stdlib/itoa.c
+++ libc/src/stdlib/itoa.c
@@ -4,24 +4,24 @@
 // Convert a value into its string representation.
 char *itoa(int value, char *buff, int base) {
 	const char digits[] = "0123456789abcdef";
-	char *s = buff;
 
-	if (value < 0 && base == 10) {
-		*s++ = '-';
+	if (value < 0) {
+		if (base == 10)
+			*buff++ = '-';
 		value *= -1;
 	}
 
 	int tmp = value;
 
 	do {
-		++s;
+		++buff;
 		tmp /= base;
 	} while (tmp > 0);
 
-	*s = '\0';
+	*buff = '\0';
 
 	do {
-		*--s = digits[value % base];
+		*--buff = digits[value % base];
 		value /= base;
 	} while (value > 0);
 
