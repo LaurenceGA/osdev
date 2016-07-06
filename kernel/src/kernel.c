077fa462a439bb8f1ec6b954e102054f63c6310e
Laurence Armstrong <lorryarmstrong@gmail.com>
2016-07-06 20:46:07 +1200

    added some printf tests

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -6,15 +6,11 @@
 int main() {
 	initTerminal();
 	printf("Welcome to Kernel World!\n");
-	printf("%s\n", "We've even got printf working!");
-	printf("Hello 0%c%x\n", 'x', 0x1af);
+	printf("%s%%\n", "We've even got printf working!");
+	printf("Hello %0#6x\n", 0x1af);
 	char s[32] = "concat";
 	printf("%s\n", strncat(s, "enated", 2));
-	printf("%s\n", strcpy(s, "copied"));
-	printf("%d\n", strcmp("aaaa", "aaab"));		// -1
-	printf("%d\n", strcmp("aaaa", "aaaa"));		// 0
-	printf("%d\n", strcmp("aaab", "aaaa"));		// 1
-	printf("%d\n", strncmp("aaaac", "aaaab", 4));	// 0
+	printf("%0+5d\n", -12);
 
 	return 0;
 }
