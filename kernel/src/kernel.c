7122986d0e552371abd9af57083770439d009315
Laurence Armstrong <lorryarmstrong@gmail.com>
2016-07-06 16:09:00 +1200

    added string.h function tests

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,21 +1,20 @@
 #include "tty.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 int main() {
 	initTerminal();
 	printf("Welcome to Kernel World!\n");
 	printf("%s\n", "We've even got printf working!");
-	printf("Hello %c\n", 'x');
-	char c[30];
-	itoa(-32000, c, 16);
-	printf("%s\n", c);
-	printf("%#x\n", 5);
-	printf("%#x\n", 0xd);
-	printf("%#x\n", 0x45f);
-	printf("%d\n", 5);
-	printf("%d\n", -12);
-	printf("%d\n", 243);
+	printf("Hello 0%c%x\n", 'x', 0x1af);
+	char s[32] = "concat";
+	printf("%s\n", strncat(s, "enated", 2));
+	printf("%s\n", strcpy(s, "copied"));
+	printf("%d\n", strcmp("aaaa", "aaab"));		// -1
+	printf("%d\n", strcmp("aaaa", "aaaa"));		// 0
+	printf("%d\n", strcmp("aaab", "aaaa"));		// 1
+	printf("%d\n", strncmp("aaaac", "aaaab", 4));	// 0
 
 	return 0;
 }
