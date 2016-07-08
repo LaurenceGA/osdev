6fd279e437ce90d31b3b29957ec5b87e1704308a
Laurence Armstrong <lorryarmstrong@gmail.com>
2016-07-08 12:18:07 +1200

    Added PIC remapping to avoid interrupt clashes

---
+++ kernel/includes/pic.h
@@ -0,0 +1,18 @@
+#ifndef PIC_H
+#define PIC_H
+
+#define PIC1_START      0x20
+#define PIC2_START      0x28
+#define PIC_NUM_IRQS    16
+
+#define PIT_INT_IDX     PIC1_START
+#define KBD_INT_IDX     PIC1_START + 1
+
+#define COM1_INT_IDX    PIC1_START + 4
+#define COM2_INT_IDX    PIC1_START + 3
+
+void pic_init();
+void pic_acknowledge();
+void pic_mask(unsigned char mask1, unsigned char mask2);
+
+#endif // PIC_H
