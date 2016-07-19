44e0cd9f958ebaab98fd8561bcab76655f1ed87a
Pyxxil <joshhill@Joshs-Air.station>
2016-07-19 23:23:55 +1200

    Added atol(l). Updated atoi so it now checks the base when checking prefix's to see if it's legal otherwise.

--- libc/includes/stdlib.h
+++ libc/includes/stdlib.h
@@ -2,6 +2,8 @@
 #define _STDLIB_H
 
 char *itoa(int, char *, int);
-int atoi(char *, int);
+int atoi(const char *, int);
+long atol(const char *, int);
+long long atoll(const char *, int);
 
 #endif // _STDLIB_H
--- libc/src/stdlib/atoll.c
+++ libc/src/stdlib/atoll.c
@@ -3,12 +3,13 @@
 
 // Convert a string representation of a number to it's base equivalent.
 // If unsuccessful, return 0.
-int atoi(const char *str, int base) {
+long long atoll(const char *str, int base) {
 	// Only support bases between 2 and 16 for now.
 	if (base > 36 || base < 2)
 		return 0;
 
-	int index, val = 0, tmp, sign = 1;
+	long long val = 0, tmp, sign = 1;
+	int index;
 
 	for (index = 0; isspace(str[index]); index++)
 		;
