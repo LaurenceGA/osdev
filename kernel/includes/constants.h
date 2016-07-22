48a692c760562fbc203c7ba04943b952d9a1657a
Pyxxil <joshhill@wifi-172-23-84-34.uoa-wifi.auckland.ac.nz>
2016-07-22 13:55:16 +1200

    Added the multiboot header, which gives us grub stuff. Constants.h contains the relevant virtual memory offset.

---
+++ kernel/includes/constants.h
@@ -0,0 +1,8 @@
+#ifndef _KCONSTANTS_H
+#define _KCONSTANTS_H
+
+// Virtual Memory stuff
+#define KERNEL_START_VADDR 	0xC0000000
+#define KERNEL_PDT_IDX		(KERNEL_START_VADDR >> 22)
+
+#endif // _KCONSTANTS_H
